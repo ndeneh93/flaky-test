4563d9c39be1584829761942354c8780b6fd6216
Ndenneh Chubu <126412465+ndeneh93@users.noreply.github.com>
2025-09-26 13:37:42 +0000

    Update TestIgniteStore.java

--- gora-ignite/src/test/java/org/apache/gora/ignite/store/TestIgniteStore.java
+++ gora-ignite/src/test/java/org/apache/gora/ignite/store/TestIgniteStore.java
@@ -17,14 +17,22 @@
  */
 package org.apache.gora.ignite.store;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+
 import org.apache.flink.shaded.guava18.com.google.common.collect.Lists;
 import org.apache.gora.ignite.GoraIgniteTestDriver;
+import org.apache.gora.query.Result;
 import org.apache.gora.store.DataStoreMetadataFactory;
 import org.apache.gora.store.DataStoreTestBase;
 import org.apache.gora.store.impl.DataStoreMetadataAnalyzer;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Test case for IgniteStore.
@@ -37,13 +45,19 @@ public class TestIgniteStore extends DataStoreTestBase {
 
   @Test
   public void igniteStoreMetadataAnalyzerTest() throws Exception {
-    DataStoreMetadataAnalyzer createAnalyzer = DataStoreMetadataFactory.createAnalyzer(DataStoreTestBase.testDriver.getConfiguration());
+    DataStoreMetadataAnalyzer createAnalyzer =
+        DataStoreMetadataFactory.createAnalyzer(DataStoreTestBase.testDriver.getConfiguration());
     Assert.assertEquals("Ignite Store Metadata Type", "IGNITE", createAnalyzer.getType());
-    Assert.assertTrue("Ignite Store Metadata Table Names", createAnalyzer.getTablesNames().equals(Lists.newArrayList("WEBPAGE", "EMPLOYEE")));
-    IgniteTableMetadata tableInfo = (IgniteTableMetadata) createAnalyzer.getTableInfo("EMPLOYEE");
-    Assert.assertEquals("Ignite Store Metadata Table Primary Key Column", "PKSSN", tableInfo.getPrimaryKey());
-    Assert.assertEquals("Ignite Store Metadata Table Primary Key Type", "VARCHAR", tableInfo.getPrimaryKeyType());
-    HashMap<String, String> hmap = new HashMap();
+    Assert.assertTrue("Ignite Store Metadata Table Names",
+        createAnalyzer.getTablesNames().equals(Lists.newArrayList("WEBPAGE", "EMPLOYEE")));
+    IgniteTableMetadata tableInfo =
+        (IgniteTableMetadata) createAnalyzer.getTableInfo("EMPLOYEE");
+    Assert.assertEquals("Ignite Store Metadata Table Primary Key Column",
+        "PKSSN", tableInfo.getPrimaryKey());
+    Assert.assertEquals("Ignite Store Metadata Table Primary Key Type",
+        "VARCHAR", tableInfo.getPrimaryKeyType());
+
+    HashMap<String, String> hmap = new HashMap<>();
     hmap.put("WEBPAGE", "VARBINARY");
     hmap.put("BOSS", "VARBINARY");
     hmap.put("SALARY", "INTEGER");
@@ -51,7 +65,27 @@ public class TestIgniteStore extends DataStoreTestBase {
     hmap.put("VALUE", "VARCHAR");
     hmap.put("NAME", "VARCHAR");
     hmap.put("SSN", "VARCHAR");
-    Assert.assertTrue("Ignite Store Metadata Table Columns", tableInfo.getColumns().equals(hmap));
+
+    Assert.assertTrue("Ignite Store Metadata Table Columns",
+        tableInfo.getColumns().equals(hmap));
   }
 
+  @Test
+  public void testQueryKeyRange() throws Exception {
+    // obtain a query result (depends on your existing setup in DataStoreTestBase)
+    Result<Long, ?> result = employeeStore.newQuery()
+        .setStartKey(2L)
+        .setEndKey(5L)
+        .execute();
+
+    List<Long> keys = new ArrayList<>();
+    while (result.next()) {
+      keys.add(result.getKey());
+    }
+
+    // FIX: enforce deterministic ordering
+    Collections.sort(keys);
+
+    assertEquals(Arrays.asList(2L, 3L, 4L), keys);
+  }
 }
